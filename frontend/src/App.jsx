19640c75036c40aebebeee5c88455e5025791c5e
trungdt1718 <trungdt1718@gmail.com>
2025-03-21 08:38:14 +0700

    Change UI and add test job

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-      <Home/>
+      <Home />
     </>
   )
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,8 @@ import product1 from '../assets/product1.webp';
 import product2 from '../assets/product2.webp';
 import product3 from '../assets/product3.webp';
 import product4 from '../assets/product4.webp';
+import product5 from '../assets/product5.webp';
+import product6 from '../assets/product6.webp';
 
 import './Home.css';
 
@@ -10,16 +12,17 @@ const Home = () => {
   return (
     <div>
       <header>
-        <div className="header-content">
-          <h1>My Shop</h1>
-          <div className="greeting">Hello Trung</div>
-        </div>
+        <div class="logo">My Shop</div>
         <nav>
+          <div class="customer-login">
+          <a href="#" class="login">Đăng nhập</a>
+            <a href="#" class="customer-service">Trung tâm CSKH</a>
+          </div>
           <ul>
-            <li><a href="/">Trang chủ</a></li>
-            <li><a href="/products">Sản phẩm</a></li>
-            <li><a href="/about">Giới thiệu</a></li>
-            <li><a href="/contact">Liên hệ</a></li>
+            <li><a href="#">Trang chủ</a></li>
+            <li><a href="#">Sản phẩm</a></li>
+            <li><a href="#">Giới thiệu</a></li>
+            <li><a href="#">Liên hệ</a></li>
           </ul>
         </nav>
       </header>
@@ -35,31 +38,40 @@ const Home = () => {
             </div>
             <div className="product">
               <img src={product2} alt="Sản phẩm 2" />
-              <h3>Quần jeans</h3>
+              <h3>Áo dài tay</h3>
               <p>Giá: 250.000 VNĐ</p>
             </div>
             <div className="product">
               <img src={product3} alt="Sản phẩm 3" />
-              <h3>Váy</h3>
+              <h3>Áo thun</h3>
               <p>Giá: 300.000 VNĐ</p>
             </div>
             <div className="product">
               <img src={product4} alt="Sản phẩm 4" />
-              <h3>Váy</h3>
-              <p>Giá: 300.000 VNĐ</p>
+              <h3>Áo thun</h3>
+              <p>Giá: 199.000 VNĐ</p>
+            </div>
+            <div className="product">
+              <img src={product5} alt="Sản phẩm 4" />
+              <h3>Áo khoác</h3>
+              <p>Giá: 299.000 VNĐ</p>
+            </div>
+            <div className="product">
+              <img src={product6} alt="Sản phẩm 4" />
+              <h3>Áo phao</h3>
+              <p>Giá: 399.000 VNĐ</p>
             </div>
           </div>
         </section>
+      </main>
 
+      <footer>
         <section>
           <h2>Về chúng tôi</h2>
           <p>
             Chúng tôi là một cửa hàng thời trang trực tuyến chuyên cung cấp các sản phẩm chất lượng cao với giá cả phải chăng.
           </p>
         </section>
-      </main>
-
-      <footer>
         <p>&copy; {new Date().getFullYear()} Cửa hàng Thời trang</p>
       </footer>
     </div>
