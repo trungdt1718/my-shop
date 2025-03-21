14cc6efaa8a07ca64696ab215024f85ad314df65
trungdt1718 <trungdt1718@gmail.com>
2025-03-21 09:45:21 +0700

    Change port backed 3000 to 5000

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = 5000;
 
 app.use(cors());
 
@@ -14,5 +14,5 @@ app.get('/names', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 })
